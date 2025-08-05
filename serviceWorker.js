e1a5fd8a58165599e603e994950791a3a8db766e
cesargil99 <cesargil99@hotmail.com>
2025-08-05 07:56:23 -0500

    Add files via upload

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,12 +1,1 @@
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('liga-de-tenis').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/app.js',
-        '/manifest.json'
-      ]);
-    })
-  );
-});
+// Service worker básico para PWA
